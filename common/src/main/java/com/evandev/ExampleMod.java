14b7411c34205978b5a6e8d45160f5649ca9f9a8
evanbones <74276616+evanbones@users.noreply.github.com>
2025-12-27 15:51:06 -0800

    Multiloader template

---
+++ common/src/main/java/com/evandev/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.evandev;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "reliable_recipes";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
