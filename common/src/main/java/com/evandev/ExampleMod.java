da6e82f242744c969bcbf0d1224698fe5f6f1307
evanbones <74276616+evanbones@users.noreply.github.com>
2025-12-27 16:32:36 -0800

    Changed my mind lol

--- common/src/main/java/com/evandev/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package com.evandev;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "reliable_recipes";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
