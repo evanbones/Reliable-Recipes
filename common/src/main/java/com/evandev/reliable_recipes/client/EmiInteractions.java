f6456b089bcc8a1941dc2e0d0ea2a1e133b96967
evanbones <74276616+evanbones@users.noreply.github.com>
2026-01-08 21:36:29 -0800

    Synthetic recipe handling

--- common/src/main/java/com/evandev/reliable_recipes/client/EmiInteractions.java
+++ common/src/main/java/com/evandev/reliable_recipes/client/EmiInteractions.java
@@ -5,10 +5,13 @@ import dev.emi.emi.api.recipe.EmiRecipe;
 import dev.emi.emi.config.EmiConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 
 public class EmiInteractions {
     public static boolean requestDeletion(EmiRecipe recipe) {
         if (recipe == null || recipe.getId() == null) return false;
+
+        ResourceLocation id = getResourceLocation(recipe);
 
         Minecraft mc = Minecraft.getInstance();
         if (mc.player == null) return false;
@@ -23,7 +26,30 @@ public class EmiInteractions {
             return false;
         }
 
-        Services.PLATFORM.sendDeleteRecipePacket(recipe.getId());
+        Services.PLATFORM.sendDeleteRecipePacket(id);
         return true;
+    }
+
+    private static ResourceLocation getResourceLocation(EmiRecipe recipe) {
+        ResourceLocation id = recipe.getId();
+
+        // Converts "jei:/modid/path/to/recipe" -> "modid:path/to/recipe"
+        if (id != null && (id.getNamespace().equals("jei") || id.getNamespace().equals("emi") || id.getNamespace().equals("toomanyrecipeviewers"))) {
+            String path = id.getPath();
+            if (path.startsWith("/")) {
+                path = path.substring(1);
+            }
+
+            int splitIndex = path.indexOf('/');
+            if (splitIndex > 0) {
+                String realNamespace = path.substring(0, splitIndex);
+                String realPath = path.substring(splitIndex + 1);
+                try {
+                    id = new ResourceLocation(realNamespace, realPath);
+                } catch (Exception ignored) {
+                }
+            }
+        }
+        return id;
     }
 }
