d7745390c2b6eb939ecd6cf4d7a57cd76afc369b
evanbones <74276616+evanbones@users.noreply.github.com>
2026-02-09 03:36:43 -0800

    Hotfix to fix possible recipe corruption

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.3.7i] - 2026-02-09
+
+### Fixed
+
+- Fix tags not applying until running /reload.
+- Fix possible issue with missing item IDs.
+
 ## [1.3.7h] - 2026-02-09
 
 ### Fixed
--- common/src/main/java/com/evandev/reliable_recipes/mixin/minecraft/ReloadableServerResourcesMixin.java
+++ common/src/main/java/com/evandev/reliable_recipes/mixin/minecraft/ReloadableServerResourcesMixin.java
@@ -1,6 +1,7 @@
 package com.evandev.reliable_recipes.mixin.minecraft;
 
 import com.evandev.reliable_recipes.recipe.RecipeModifier;
+import com.evandev.reliable_recipes.recipe.TagModifier;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.server.ReloadableServerResources;
 import net.minecraft.world.item.crafting.RecipeManager;
@@ -23,6 +24,7 @@ public class ReloadableServerResourcesMixin {
             at = @At("RETURN")
     )
     private void reliableRecipes$onTagsLoaded(RegistryAccess registryAccess, CallbackInfo ci) {
+        TagModifier.apply();
         RecipeModifier.apply(this.recipes);
     }
 }
