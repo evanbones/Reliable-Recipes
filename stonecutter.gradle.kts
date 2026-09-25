32fb9894fbcfa664c525dbe2a63a9513f526e820
evanbones <74276616+evanbones@users.noreply.github.com>
2026-09-25 16:34:44 -0700

    bump loom (please work please work plllelasase)

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("dev.kikugie.stonecutter")
-    id("net.fabricmc.fabric-loom") version "1.15-SNAPSHOT" apply false
+    id("net.fabricmc.fabric-loom") version "1.17-SNAPSHOT" apply false
     id("net.neoforged.moddev") version "2.0.141" apply false
     id("dev.kikugie.postprocess.jsonlang") version "2.1-beta.4" apply false
     id("me.modmuss50.mod-publish-plugin") version "2.2.1" apply false
